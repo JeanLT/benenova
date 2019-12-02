927fa511ff792cda2e15f9335e6b19d1866e10bd
Jean Le Tulzo <jean.letulzo@gmail.com>
2019-12-02 19:09:01 +0100

    model-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  has_many :missions, through: :bookings, 
+
+  validates :first_name, uniqueness: { scope: :last_name }
+  validates :last_name, uniqueness: { scope: :first_name }
+  validates :address, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :birth, uniqueness: true, presence: true
+  validates :phone_number, uniqueness: true, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
