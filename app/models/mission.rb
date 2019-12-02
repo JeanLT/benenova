4ffd9fc94b675bb1f914e595c4794009dfa3c8e8
Jean Le Tulzo <jean.letulzo@gmail.com>
2019-12-02 18:59:44 +0100

    model - mission

---
+++ app/models/mission.rb
@@ -0,0 +1,19 @@
+class Mission < ApplicationRecord
+	belongs_to :association
+	belongs_to :partenaire
+	has_many :bookings
+	has_many :users, through: :bookings
+
+	validates :title, presence: true
+	validates :city, presence: true
+	validates :datetime, presence: true
+	validates :duration, presence: true
+	validates :description, presence: true
+	validates :picture, presence: true
+	validates :cause, presence: true
+	validates :address, presence: true
+	validates :max_capacity, presence: true
+	validates :min_capacity, presence: true
+	validates :partenaire, presence: true
+
+end
