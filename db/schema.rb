93cfb5d6512258ac19476fdb2407a3d1af1491ff
Jean Le Tulzo <jean.letulzo@gmail.com>
2019-12-03 13:31:16 +0100

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_103601) do
+ActiveRecord::Schema.define(version: 2019_12_03_112257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "number_volunteers"
+    t.string "status"
+    t.bigint "user_id"
+    t.bigint "mission_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mission_id"], name: "index_bookings_on_mission_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "missions", force: :cascade do |t|
     t.string "city"
@@ -65,6 +76,8 @@ ActiveRecord::Schema.define(version: 2019_12_03_103601) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "missions"
+  add_foreign_key "bookings", "users"
   add_foreign_key "missions", "partners"
   add_foreign_key "missions", "users", column: "supernova_id"
 end
