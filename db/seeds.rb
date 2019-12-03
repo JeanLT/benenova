1534ab58d0909b722fb46eb5d3b1e3359d82cc2f
Jean Le Tulzo <jean.letulzo@gmail.com>
2019-12-03 15:19:44 +0100

    seeds - pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,28 +13,36 @@ puts "Destroy missions"
 Costume.destroy_all
 
 puts 'Creating users...'
-user1 = User.new(email: "", password: "secret", first_name: "Théo", last_name: "Leroy", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user1.remote_image_url = "https://topcomics.fr/wp-content/uploads/2019/09/classement-des-20-superheroines-dc-comics-les-plus-sexy-mera.jpg"
+user1 = User.new(email: "theo.leroy@exemple.com", password: "secret", first_name: "Théo", last_name: "Leroy", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://www.flickr.com/photos/benenovanantes/18833309065/"
 user1.save!
-user2 = User.new(email: "", password: "secret", first_name: "Manon", last_name: "Morel", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user2.save!
-user3 = User.new(email: "", password: "secret", first_name: "Lucas", last_name: "Fournier", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user3.save!
-user4 = User.new(email: "", password: "secret", first_name: "Camille", last_name: "#", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user4.save!
-user5 = User.new(email: "", password: "secret", first_name: "Julien", last_name: "Girard", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user5.save!
-user6 = User.new(email: "", password: "secret", first_name: "Emma", last_name: "Boyer", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user6.save!
-user7 = User.new(email: "", password: "secret", first_name: "Nicolas", last_name: "Chevallier", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user7.save!
-user8 = User.new(email: "", password: "secret", first_name: "Marie", last_name: "Muller", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user8.save!
-user9 = User.new(email: "", password: "secret", first_name: "Alexandre", last_name: "Lemaire", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user9.save!
-user10 = User.new(email: "", password: "secret", first_name: "Sarah", last_name: "Berger", avatar: "secret", address: "#", birth: "#", favorite_city: "", favorite_cause: "", phone_number: "")
-user10.save!
-
+user1 = User.new(email: "sarah.lambert@exemple.com", password: "secret", first_name: "Sarah", last_name: "Lambert", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://www.flickr.com/photos/benenovanantes/18212617333/"
+user1.save!
+user1 = User.new(email: "david.fournier@exemple.com", password: "secret", first_name: "David", last_name: "Fournier", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://visualhunt.com/f3/photo/4327907871/ae32192cb1/"
+user1.save!
+user1 = User.new(email: "lucie.guerin@exemple.com", password: "secret", first_name: "Lucie", last_name: "Guerin", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://www.flickr.com/photos/benenovanantes/18833309065/"
+user1.save!
+user1 = User.new(email: "laeticia.blanc@exemple.com", password: "secret", first_name: "Laeticia", last_name: "Blanc", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://visualhunt.com/f3/photo/2363332757/997502c729/"
+user1.save!
+user1 = User.new(email: "quentin.duval@exemple.com", password: "secret", first_name: "Quentin", last_name: "Duval", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://visualhunt.com/f3/photo/15095552502/9bc640be65/"
+user1.save!
+user1 = User.new(email: "jennifer.fabre@exemple.com", password: "secret", first_name: "Jennifer", last_name: "Fabre", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://topcomics.fr/wp-content/uploads/2019/09/classement-des-20-superheroines-dc-comics-les-plus-sexy-mera.jpg"
+user1.save!
+user1 = User.new(email: "sophie.huet@exemple.com", password: "secret", first_name: "Sophie", last_name: "Huet", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://www.flickr.com/photos/benenovanantes/16680242149/"
+user1.save!
+user1 = User.new(email: "mathilde.perez@exemple.com", password: "secret", first_name: "Mathilde", last_name: "Perez", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://visualhunt.com/f3/photo/132530133/b7e7bc9fd7/"
+user1.save!
+user1 = User.new(email: "florent.aubry@exemple.com", password: "secret", first_name: "Florent", last_name: "Aubry", avatar: "#", address: "8 rue du Haut Moreau", birth: "31/12/1984", favorite_city: "Nantes", favorite_cause: "Arts,culture et loisirs", phone_number: "0291923848")
+user1.remote_avatar_url = "https://visualhunt.com/f3/photo/14960459339/1ce520883e/"
+user1.save!
 
 
 
