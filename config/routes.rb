08ce79c5c75e800f73af7ad371ef5de4fbba4990
Jean Le Tulzo <jean.letulzo@gmail.com>
2019-12-04 11:19:14 +0100

    end of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :missions, only:[:show, :index] do
+    resources :bookings, only:[:create]   
+  end
+
+  get "/dashboard", to: "users#dashboard"
+  # resource :dashboard, only: :show
+  # => GET   /dashboard(.:format)   dashboards#show
+
+  resources :bookings, only: [] do
+  	member do
+  		patch "cancel"
+  	end 
+	end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
