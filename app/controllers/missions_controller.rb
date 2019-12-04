1503c3e5c005b6d0bfe293a46fd7b309e2afd6b6
Paola Carbonnel paolacarbonnel@hotmail.com <paolacarbonnel@hotmail.com>
2019-12-04 18:58:12 +0100

    add custom map

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -2,9 +2,19 @@ class MissionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @missions = Mission.all
+    @missions = Massion.geocoded
+
+    @markers = @missions.map do |mission|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat }),
+        image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+      }
+    end
   end
 
   def show
-
   end
+
 end
