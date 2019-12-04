bbfb4a892be92a4131f2ab9865eafe38e583bf32
Tiffany Loue <tiffany.loue@gmail.com>
2019-12-04 18:59:23 +0100

    index en cours

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -7,11 +7,24 @@ module MissionsHelper
     return "#{date} - #{start_time} à #{end_time}"
   end
 
-  def display_picto_cause(mission)
-    case picto
-    when mission. cause == "Handicap"
-      return image_tag
-      return "Handicap"
-
+  def display_picto_cause(cause)
+    case cause
+    when "Handicap"
+      return image_tag("Handicap.png")
+    when "Environnement"
+      return image_tag("Environnement.png")
+    when "Précarité"
+      return image_tag("Precarite.png")
+    when "Lien intergénérationnel"
+      return image_tag("Lien intergenerationnel.png")
+    when "Education"
+      return image_tag("Education.png")
+    when "Arts, culture et loisirs"
+      return image_tag("Arts, culture et loisirs.png")
+    end
   end
+
+  # def capacity(bookings, maxcapacity)
+  #   capacity = maxcapacity - bookings.count
+  # end
 end
