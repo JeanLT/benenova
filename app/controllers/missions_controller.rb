2e0f89a971ca7437cba12f856fa7ab3b093e866e
Paola Carbonnel paolacarbonnel@hotmail.com <paolacarbonnel@hotmail.com>
2019-12-05 12:21:23 +0100

    map's window

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,15 +1,14 @@
 class MissionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @missions = Mission.all
-    @missions = Massion.geocoded
+    @missions = Mission.geocoded
 
     @markers = @missions.map do |mission|
       {
-        lat: flat.latitude,
-        lng: flat.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat }),
-        image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+        lat: mission.latitude,
+        lng: mission.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { mission: mission })
+        # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
       }
     end
   end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,17 +8,14 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
+      dragPan: true,
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
+    addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                       mapboxgl: mapboxgl }));
-    });
   }
 };
 
@@ -32,14 +29,14 @@ const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
         // Create a HTML element for your custom marker
-    const element = document.createElement('div');
-    element.className = 'marker';
-    element.style.backgroundImage = `url('${marker.image_url}')`;
-    element.style.backgroundSize = 'contain';
-    element.style.width = '25px';
-    element.style.height = '25px';
+    // const element = document.createElement('div');
+    // element.className = 'marker';
+    // element.style.backgroundImage = `url('${marker.image_url}')`;
+    // element.style.backgroundSize = 'contain';
+    // element.style.width = '25px';
+    // element.style.height = '25px';
 
-    new mapboxgl.Marker(element)
+    new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
       .addTo(map);
