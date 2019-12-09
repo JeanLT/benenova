18a82494aba65b7315430a86ee0febce9bbd8c30
Paola Carbonnel paolacarbonnel@hotmail.com <paolacarbonnel@hotmail.com>
2019-12-09 17:14:14 +0100

    navs add to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def dashboard
     @bookings = current_user.bookings
-    @booking_realised = current_user.bookings.joins(:mission).where("missions.datetime < ?", Date.today)
-    @booking_to_come = current_user.bookings.joins(:mission).where("missions.datetime >= ?", Date.today)
+    @bookings_realised = current_user.bookings.joins(:mission).where("missions.datetime < ?", Date.today)
+    @bookings_to_come = current_user.bookings.joins(:mission).where("missions.datetime >= ?", Date.today)
   end
 end
