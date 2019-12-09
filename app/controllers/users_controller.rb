fd5791011d30f2279cf2c701bf089e528e2d80a3
Paola Carbonnel paolacarbonnel@hotmail.com <paolacarbonnel@hotmail.com>
2019-12-09 18:51:27 +0100

    datetime asc ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def dashboard
     @bookings = current_user.bookings
-    @bookings_realised = current_user.bookings.joins(:mission).where("missions.datetime < ?", Date.today)
-    @bookings_to_come = current_user.bookings.joins(:mission).where("missions.datetime >= ?", Date.today)
+    @bookings_realised = current_user.bookings.joins(:mission).where("missions.datetime < ?", Date.today).order(datetime: :asc)
+    @bookings_to_come = current_user.bookings.joins(:mission).where("missions.datetime >= ?", Date.today).order(datetime: :asc)
   end
 end
