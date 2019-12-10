6b94a43d0f20e66f29bf9206360b451813ab7545
Tiffany Loue <tiffany.loue@gmail.com>
2019-12-10 14:41:37 +0100

    slider in progress

---
+++ app/javascript/plugins/init_siema.js
@@ -0,0 +1,16 @@
+import Siema from 'siema';
+
+new Siema({
+  selector: '.cards-container',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: false,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
